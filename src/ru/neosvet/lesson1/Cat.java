396346e5febaa34e3ad7387195e35abd4cff1e41
Neosvet <neosvet333@gmail.com>
2020-11-10 13:45:09 +0300

    added '@Override'

--- src/ru/neosvet/lesson1/Cat.java
+++ src/ru/neosvet/lesson1/Cat.java
@@ -5,30 +5,37 @@ public class Cat implements Entity {
     private final int LIMIT_HEIGHT = 4;
     private boolean lose = false;
 
+    @Override
     public void run() {
         System.out.println("Кот пробежал.");
     }
 
+    @Override
     public void jump() {
         System.out.println("Кот перепрыгнул.");
     }
 
+    @Override
     public boolean canRun(int distance) {
         return distance <= LIMIT_DISTANCE;
     }
 
+    @Override
     public boolean canJump(int height) {
         return height <= LIMIT_HEIGHT;
     }
 
+    @Override
     public String getType() {
         return "Кот";
     }
 
+    @Override
     public void setLose(boolean lose) {
         this.lose = lose;
     }
 
+    @Override
     public boolean isLose() {
         return lose;
     }
--- src/ru/neosvet/lesson1/Robot.java
+++ src/ru/neosvet/lesson1/Robot.java
@@ -5,30 +5,37 @@ public class Robot implements Entity {
     private final int LIMIT_HEIGHT = 1;
     private boolean lose = false;
 
+    @Override
     public void run() {
         System.out.println("Робот пробежал.");
     }
 
+    @Override
     public void jump() {
         System.out.println("Робот перепрыгнул.");
     }
 
+    @Override
     public boolean canRun(int distance) {
         return distance <= LIMIT_DISTANCE;
     }
 
+    @Override
     public boolean canJump(int height) {
         return height <= LIMIT_HEIGHT;
     }
 
+    @Override
     public String getType() {
         return "Робот";
     }
 
+    @Override
     public void setLose(boolean lose) {
         this.lose = lose;
     }
 
+    @Override
     public boolean isLose() {
         return lose;
     }
