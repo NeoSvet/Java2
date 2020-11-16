793a712ece3f15579aa3c6ad2241bb640ca2de8f
Neosvet <neosvet333@gmail.com>
2020-11-16 13:54:27 +0300

    lesson 2, task 1

--- src/ru/neosvet/lesson2/Main.java
+++ src/ru/neosvet/lesson2/Main.java
@@ -1,7 +1,55 @@
 package ru.neosvet.lesson2;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Main {
+    private static final int SIZE = 4;
+
+    public static void main(String[] args) {
+        String[][] array = getCorrectArray();
+        printArray(array);
+        checkSize(array);
+        array = getWrongSizeArray();
+        printArray(array);
+        checkSize(array);
+    }
+
+    private static void checkSize(String[][] array) {
+        if (array.length != SIZE) {
+            System.out.println("MyArraySizeException");
+            return;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != SIZE) {
+                System.out.println("MyArraySizeException");
+                return;
+            }
+        }
+    }
+
+    private static void printArray(String[][] array) {
+        System.out.println("Вывод массива:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(Arrays.toString(array[i]));
+        }
+    }
+
+    private static String[][] getCorrectArray() {
+        return new String[][]{
+                {"2", "5", "77", "34"},
+                {"57", "83", "8", "1"},
+                {"23", "19", "67", "7"},
+                {"29", "50", "3", "49"}
+        };
+    }
+
+    private static String[][] getWrongSizeArray() {
+        return new String[][]{
+                {"2", "5", "77", "34", "33"},
+                {"57", "83", "8", "1"},
+                {"23", "19", "67", "7"},
+                {"29", "50", "3", "49"},
+                {"29", "50", "3", "49"}
+        };
     }
 }
