c20838b0b5a14ba22c20e3f88db20a7bcc6799be
Neosvet <neosvet333@gmail.com>
2020-11-27 23:30:02 +0300

    added twoThread

--- src/ru/neosvet/lesson5/Main.java
+++ src/ru/neosvet/lesson5/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         TestThread test = new TestThread();
         test.oneThread();
+        test.twoThread();
     }
 }
--- src/ru/neosvet/lesson5/TestThread.java
+++ src/ru/neosvet/lesson5/TestThread.java
@@ -3,9 +3,12 @@ package ru.neosvet.lesson5;
 import java.util.Arrays;
 
 public class TestThread {
-    private static final int size = 10000000;
-    private static final int h = size / 2;
-    private static float[] arr = new float[size];
+    private static final int SIZE = 10000000;
+    private static final int HALF_SIZE = SIZE / 2;
+    private static final int THREAD_MAX = 2;
+    private static float[] arr = new float[SIZE];
+    private int thread_stoped = 0;
+    private long start_time;
 
     public void oneThread() {
         Arrays.fill(arr, 1f);
@@ -16,6 +19,27 @@ public class TestThread {
                 arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
             System.out.println("Time for OneThread: " + (System.currentTimeMillis() - start_time));
+        });
+        thread.start();
+    }
+
+    public void twoThread() {
+        Arrays.fill(arr, 1f);
+        System.out.println("TwoThread is started...");
+        start_time = System.currentTimeMillis();
+        startThread(Arrays.copyOfRange(arr, 0, HALF_SIZE), 0);
+        startThread(Arrays.copyOfRange(arr, HALF_SIZE, SIZE), HALF_SIZE);
+    }
+
+    private void startThread(float[] arr, int offset) {
+        Thread thread = new Thread(() -> {
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = (float) (arr[i] * Math.sin(0.2f + ((i + offset) / 5)) *
+                        Math.cos(0.2f + ((i + offset) / 5)) * Math.cos(0.4f + ((i + offset) / 2)));
+            }
+            thread_stoped++;
+            if (thread_stoped == THREAD_MAX)
+                System.out.println("Time for TwoThread: " + (System.currentTimeMillis() - start_time));
         });
         thread.start();
     }
