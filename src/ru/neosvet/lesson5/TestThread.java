585c3eb47e947cc308a12b3a78c492e5394cdca9
Neosvet <neosvet333@gmail.com>
2020-11-27 23:47:55 +0300

    added glueArrs

--- src/ru/neosvet/lesson5/TestThread.java
+++ src/ru/neosvet/lesson5/TestThread.java
@@ -6,7 +6,8 @@ public class TestThread {
     private static final int SIZE = 10000000;
     private static final int HALF_SIZE = SIZE / 2;
     private static final int THREAD_MAX = 2;
-    private static float[] arr = new float[SIZE];
+    private float[] arr = new float[SIZE];
+    private float[] arr1, arr2;
     private int thread_stoped = 0;
     private long start_time;
 
@@ -27,8 +28,10 @@ public class TestThread {
         Arrays.fill(arr, 1f);
         System.out.println("TwoThread is started...");
         start_time = System.currentTimeMillis();
-        startThread(Arrays.copyOfRange(arr, 0, HALF_SIZE), 0);
-        startThread(Arrays.copyOfRange(arr, HALF_SIZE, SIZE), HALF_SIZE);
+        arr1 = Arrays.copyOfRange(arr, 0, HALF_SIZE);
+        startThread(arr1, 0);
+        arr2 = Arrays.copyOfRange(arr, HALF_SIZE, SIZE);
+        startThread(arr2, HALF_SIZE);
     }
 
     private void startThread(float[] arr, int offset) {
@@ -38,9 +41,16 @@ public class TestThread {
                         Math.cos(0.2f + ((i + offset) / 5)) * Math.cos(0.4f + ((i + offset) / 2)));
             }
             thread_stoped++;
-            if (thread_stoped == THREAD_MAX)
+            if (thread_stoped == THREAD_MAX) {
+                glueArrs();
                 System.out.println("Time for TwoThread: " + (System.currentTimeMillis() - start_time));
+            }
         });
         thread.start();
+    }
+
+    private void glueArrs() {
+        System.arraycopy(arr1, 0, arr, 0, HALF_SIZE);
+        System.arraycopy(arr2, 0, arr, HALF_SIZE, HALF_SIZE);
     }
 }
